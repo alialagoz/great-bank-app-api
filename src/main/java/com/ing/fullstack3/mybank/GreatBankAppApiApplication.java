fd008d149c8fe92d172ab536a2cfb9d770d6ffe5
Ali Alagoz <ali.alagoz@ing.com>
2020-09-15 15:12:18 +0200

    first commit

---
+++ src/main/java/com/ing/fullstack3/mybank/GreatBankAppApiApplication.java
@@ -0,0 +1,13 @@
+package com.ing.fullstack3.mybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GreatBankAppApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GreatBankAppApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ing/fullstack3/mybank/GreatBankAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ing.fullstack3.mybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GreatBankAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
